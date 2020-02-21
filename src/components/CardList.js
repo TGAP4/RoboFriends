52d843126cfabaa8ec434de91038f557c50e889d
TGAP4 <60671900+TGAP4@users.noreply.github.com>
2020-02-21 05:05:50 -0500

    added a way to remove titles

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,11 +2,21 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
+
+    robots.map((user, i) => {
+        if (robots[i].name.toLowerCase().startsWith('mr. ') || robots[i].name.toLowerCase().startsWith('ms. ')) {
+            robots[i].name = robots[i].name.slice(4);
+        } else if (robots[i].name.toLowerCase().startsWith('mrs. ')) {
+            robots[i].name = robots[i].name.slice(5);
+        }
+        return robots;
+    });
+
     return (
         <div className='tc'>
             {
                 robots.sort((a,b) => a.name > b.name ? 1 : -1)
-                .map((user, i) => ( 
+                .map((user, i) => (
                     <Card
                     key={i} 
                     id={robots[i].id} 
